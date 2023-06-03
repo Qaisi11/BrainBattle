b2beb92d72de0431e860faf4deffe897c25e6fad
Qaisi11 <iqaisi.26@acsamman.edu.jo>
2023-06-03 21:07:40 +0300

    Commit

--- script.js
+++ script.js
@@ -1,20 +1,45 @@
-// Add your JavaScript code here
-
-// Milestone 1: Basic quiz format
-
-// Sample quiz data (replace with your own)
 const quizData = [
     {
-      question: "What is the capital of France?",
+      question: "What is the capital of FRANCE?",
       options: ["Paris", "London", "Berlin", "Rome"],
       correctAnswer: "Paris"
     },
     {
-      question: "Who painted the Mona Lisa?",
+      question: "Who painted the MONA LISA?",
       options: ["Leonardo da Vinci", "Pablo Picasso", "Vincent van Gogh", "Michelangelo"],
       correctAnswer: "Leonardo da Vinci"
     },
-    // Add more questions here...
+    {
+      question: "What is the capital of Canada?",
+      options: ["Toronto", "Vancouver", "Montreal", "Ottawa"],
+      correctAnswer: "Ottawa"
+    },
+    {
+      question: "What is the largest planet in our solar system?",
+      options: ["Mercury", "Venus", "Earth", "Jupiter"],
+      correctAnswer: "Jupiter"
+    },
+    {
+      question: "What is the largest organ in the human body?",
+      options: ["Liver", "Heart", "Lungs", "Skin"],
+      correctAnswer: "Skin"
+    },
+    {
+      question: "Who invented the telephone?",
+      options: ["Thomas Edison", "Alexander Graham Bell", "Nikola Tesla", "Benjamin Franklin"],
+      correctAnswer: "Alexander Graham Bell"
+    },
+    {
+      question: "What is the national animal of Canada?",
+      options: ["Bald eagle", "Beaver", "Grizzly bear", "Moose"],
+      correctAnswer: "Beaver"
+    },
+    {
+      question: "What is the tallest mammal in the world?",
+      options: ["Giraffe", "Elephant", "Hippopotamus", "Rhino"],
+      correctAnswer: "Giraffe"
+    }
+  
   ];
   
   // Display quiz questions
