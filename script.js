3e11eeaa740e826ea88b04705b24ed6160ce64cd
Qaisi11 <iqaisi.26@acsamman.edu.jo>
2023-06-05 09:23:46 +0300

    Commit

--- script.js
+++ script.js
@@ -1,80 +1,3 @@
-const quizData = [
-    {
-      question: "What is the capital of France?",
-      options: ["Paris", "London", "Berlin", "Rome"],
-      correctAnswer: "Paris"
-    },
-    {
-      question: "Who painted the Mona Lisa?",
-      options: ["Leonardo da Vinci", "Pablo Picasso", "Vincent van Gogh", "Michelangelo"],
-      correctAnswer: "Leonardo da Vinci"
-    },
-    {
-      question: "What is the capital of Canada?",
-      options: ["Toronto", "Vancouver", "Montreal", "Ottawa"],
-      correctAnswer: "Ottawa"
-    },
-    {
-      question: "What is the largest planet in our solar system?",
-      options: ["Mercury", "Venus", "Earth", "Jupiter"],
-      correctAnswer: "Jupiter"
-    },
-    {
-      question: "What is the largest organ in the human body?",
-      options: ["Liver", "Heart", "Lungs", "Skin"],
-      correctAnswer: "Skin"
-    },
-    {
-      question: "Who invented the telephone?",
-      options: ["Thomas Edison", "Alexander Graham Bell", "Nikola Tesla", "Benjamin Franklin"],
-      correctAnswer: "Alexander Graham Bell"
-    },
-    {
-      question: "What is the national animal of Canada?",
-      options: ["Bald eagle", "Beaver", "Grizzly bear", "Moose"],
-      correctAnswer: "Beaver"
-    },
-    {
-      question: "What is the tallest mammal in the world?",
-      options: ["Giraffe", "Elephant", "Hippopotamus", "Rhino"],
-      correctAnswer: "Giraffe"
-    }
-  ];
-  
-  const quizData2 = [
-    {
-      question: "What is the capital of Japan?",
-      options: ["Tokyo", "Seoul", "Beijing", "Bangkok"],
-      correctAnswer: "Tokyo"
-    },
-    {
-      question: "Who wrote the play 'Romeo and Juliet'?",
-      options: ["William Shakespeare", "Jane Austen", "Charles Dickens", "Mark Twain"],
-      correctAnswer: "William Shakespeare"
-    },
-    {
-      question: "What is the currency of Australia?",
-      options: ["Dollar", "Pound", "Euro", "Yen"],
-      correctAnswer: "Dollar"
-    },
-    {
-      question: "Which country is known as the 'Land of the Rising Sun'?",
-      options: ["China", "India", "Japan", "Australia"],
-      correctAnswer: "Japan"
-    },
-    {
-      question: "Who painted the famous artwork 'The Starry Night'?",
-      options: ["Pablo Picasso", "Vincent van Gogh", "Leonardo da Vinci", "Claude Monet"],
-      correctAnswer: "Vincent van Gogh"
-    },
-    {
-      question: "What is the largest ocean on Earth?",
-      options: ["Pacific Ocean", "Atlantic Ocean", "Indian Ocean", "Arctic Ocean"],
-      correctAnswer: "Pacific Ocean"
-    }
-  
-  ];
-
 const quizContainer = document.getElementById("quiz-container");
 const quizFeedback = document.getElementById("quiz-feedback");
 const submitButton = document.getElementById("submit-button");
@@ -194,3 +117,81 @@ answerOptions.forEach(option => {
     }
   });
 });
+const quizData = [
+    {
+      question: "What is the capital of France?",
+      options: ["Paris", "London", "Berlin", "Rome"],
+      correctAnswer: "Paris"
+    },
+    {
+      question: "Who painted the Mona Lisa?",
+      options: ["Leonardo da Vinci", "Pablo Picasso", "Vincent van Gogh", "Michelangelo"],
+      correctAnswer: "Leonardo da Vinci"
+    },
+    {
+      question: "What is the capital of Canada?",
+      options: ["Toronto", "Vancouver", "Montreal", "Ottawa"],
+      correctAnswer: "Ottawa"
+    },
+    {
+      question: "What is the largest planet in our solar system?",
+      options: ["Mercury", "Venus", "Earth", "Jupiter"],
+      correctAnswer: "Jupiter"
+    },
+    {
+      question: "What is the largest organ in the human body?",
+      options: ["Liver", "Heart", "Lungs", "Skin"],
+      correctAnswer: "Skin"
+    },
+    {
+      question: "Who invented the telephone?",
+      options: ["Thomas Edison", "Alexander Graham Bell", "Nikola Tesla", "Benjamin Franklin"],
+      correctAnswer: "Alexander Graham Bell"
+    },
+    {
+      question: "What is the national animal of Canada?",
+      options: ["Bald eagle", "Beaver", "Grizzly bear", "Moose"],
+      correctAnswer: "Beaver"
+    },
+    {
+      question: "What is the tallest mammal in the world?",
+      options: ["Giraffe", "Elephant", "Hippopotamus", "Rhino"],
+      correctAnswer: "Giraffe"
+    }
+  ];
+
+
+  const quizData2 = [
+    {
+      question: "What is the capital of Japan?",
+      options: ["Tokyo", "Seoul", "Beijing", "Bangkok"],
+      correctAnswer: "Tokyo"
+    },
+    {
+      question: "Who wrote the play 'Romeo and Juliet'?",
+      options: ["William Shakespeare", "Jane Austen", "Charles Dickens", "Mark Twain"],
+      correctAnswer: "William Shakespeare"
+    },
+    {
+      question: "What is the currency of Australia?",
+      options: ["Dollar", "Pound", "Euro", "Yen"],
+      correctAnswer: "Dollar"
+    },
+    {
+      question: "Which country is known as the 'Land of the Rising Sun'?",
+      options: ["China", "India", "Japan", "Australia"],
+      correctAnswer: "Japan"
+    },
+    {
+      question: "Who painted the famous artwork 'The Starry Night'?",
+      options: ["Pablo Picasso", "Vincent van Gogh", "Leonardo da Vinci", "Claude Monet"],
+      correctAnswer: "Vincent van Gogh"
+    },
+    {
+      question: "What is the largest ocean on Earth?",
+      options: ["Pacific Ocean", "Atlantic Ocean", "Indian Ocean", "Arctic Ocean"],
+      correctAnswer: "Pacific Ocean"
+    }
+  
+  ];
+
